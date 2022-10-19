b02d03bec894ace06725c2b281539552736461ad
Florian Weiss <florian.weiss@weissheiten.at>
2022-10-19 11:19:47 +0200

    Introduce .gitignore, elaborate on task info

--- index.php
+++ index.php
@@ -16,6 +16,10 @@
 // AUFGABE 4
 // Legen Sie ein Repository auf einem VC Server Ihrer Wahl an (zB GitHub, GitLab, etc.) und pushen Sie das Ergebnis
 // auf den Branch "main"
+// Sie können entweder die Files (ohne .git Verzeichnis) in ein neues Directory kopieren und von dort arbeiten oder
+// einfacher mit dem Befehl
+// git remote set-url <remote_name> <remote_url>
+// die Ziel URL des Repositories ändern
 
 // AUFGABE 5
 // Taggen Sie den funktionalen Code mit der Version v1.0.0 in Ihrem Repository
